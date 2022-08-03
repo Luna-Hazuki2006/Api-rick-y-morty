3a8aec5499ac89238fe0ba2c6afc27983fca3279
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-03 17:13:36 -0400

    aaaaaa

--- capitulos/index.js
+++ capitulos/index.js
@@ -1,9 +1,16 @@
 const init = async () => {
     try {
-        
+        const response = await fetch("https://rickandmortyapi.com/api/episode")
+        const data = await response.json()
+        const charContenedorElement = document.getElementById("char-contenedor")
+        charContenedorElement.innerHTML = ""
+        data.results.forEach(element => {
+            
+        });  
     } catch (error) {
         console.error(error)
         console.log("Hubo un error");
+        
     }
 }
 
