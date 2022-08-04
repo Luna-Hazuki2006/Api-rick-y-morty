1cb7e26237d2cd9f09040acccc6b68986491cf94
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-04 08:15:55 -0400

    practicamente  hecho

--- capitulos/index.js
+++ capitulos/index.js
@@ -2,15 +2,28 @@ const init = async () => {
     try {
         const response = await fetch("https://rickandmortyapi.com/api/episode")
         const data = await response.json()
+        console.log(data);
         const charContenedorElement = document.getElementById("char-contenedor")
         charContenedorElement.innerHTML = ""
-        data.results.forEach(element => {
-            
+        data.results.forEach((element) => {
+            charContenedorElement.innerHTML += `
+            <div class="char-car">
+                <div class="char-info">
+                    <p>${element["name"]}</p>
+                </div>
+                <div class="char-info">
+                    <p>${element["air_date"]}</p>
+                </div>
+                <div class="char-info">
+                    <p>${element["episode"]}</p>
+                </div>
+            </div>
+            `
         });  
+
     } catch (error) {
         console.error(error)
         console.log("Hubo un error");
-        
     }
 }
 
